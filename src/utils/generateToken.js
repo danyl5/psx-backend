91e1ad108cc298cdccd8c598856af262a0677174
Daniyal <daniyal.mughees@systemsltd.com>
2026-07-22 13:36:53 +0500

    Update generateToken.js

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (userId) =>
   jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "2h"
+    expiresIn: "2d"
   });
 
 export default generateToken;
