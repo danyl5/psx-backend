3a5d99fe54f7850d110be1cfba77240f14cdc5e2
Daniyal <daniyal.mughees@systemsltd.com>
2026-07-29 01:56:56 +0500

    Update psxService.js

--- src/services/psxService.js
+++ src/services/psxService.js
@@ -83,15 +83,6 @@ export async function fetchStockPriceFromPSX(symbol) {
     };
   } catch (error) {
     console.error("Error fetching PSX price for", symbol, error.message);
-    console.log({
-      message: error.message,
-      code: error.code,
-      errno: error.errno,
-      syscall: error.syscall,
-      address: error.address,
-      port: error.port,
-      status: error.response?.status,
-    });
     return { symbol, price: null };
   }
 }
